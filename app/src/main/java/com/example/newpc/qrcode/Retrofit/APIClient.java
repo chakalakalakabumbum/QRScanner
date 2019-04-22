da94d3b7aaca99e12adc7abf1cf72073a592720f
Dominator <nuclearbomb1995@gmail.com>
2019-04-22 23:49:25 +0700

    First release

---
+++ app/src/main/java/com/example/newpc/qrcode/Retrofit/APIClient.java
@@ -0,0 +1,159 @@
+package com.example.newpc.qrcode.Retrofit;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.util.Base64;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.internal.LinkedTreeMap;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    public static final int REQUEST_CODE_PICK_IMAGE = 1001;
+
+    private static final String AUTHORIZATION_URL = "https://api.imgur.com/oauth2/authorize";
+    private static final String CLIENT_ID = "9526317f2afe001";
+    private static final String CLIENT_SECRET = "6657db8085e9f9af37194ab8579bb93810c0db0e";
+    private OkHttpClient httpClient;
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String link) {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.NONE);
+
+        // Creates the json object which will manage the information received
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        // Register an adapter to manage the date types as long values
+        gsonBuilder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
+
+        Gson gson = gsonBuilder.setLenient().create();
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(1, TimeUnit.MINUTES) // connect timeout
+                .writeTimeout(1, TimeUnit.MINUTES) // write timeout
+                .readTimeout(1, TimeUnit.MINUTES); // read timeout
+
+        OkHttpClient client = builder.addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(link)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+
+
+        return retrofit;
+    }
+
+    private static class ByteArrayToBase64TypeAdapter implements JsonSerializer<byte[]>, JsonDeserializer<byte[]> {
+        public byte[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return Base64.decode(json.getAsString(), Base64.NO_WRAP);
+        }
+
+        public JsonElement serialize(byte[] src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(Base64.encodeToString(src, Base64.NO_WRAP));
+        }
+    }
+
+    public Object ObjectConverter(Object treeMapResponse, Object convertClass){
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        // Register an adapter to manage the date types as long values
+        gsonBuilder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
+
+        gsonBuilder.registerTypeHierarchyAdapter(byte[].class,
+                new ByteArrayToBase64TypeAdapter());
+
+
+        Gson gson = gsonBuilder.setLenient().create();
+
+        if(treeMapResponse.getClass().equals(LinkedTreeMap.class)) {
+            return gson.fromJson(gson.toJson((LinkedTreeMap) treeMapResponse), convertClass.getClass());
+        }
+        else{
+            return gson.fromJson(gson.toJson(treeMapResponse), convertClass.getClass());
+        }
+    }
+
+    public static int getResId(String resName, Class<?> c) {
+
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
+            int targetWidth = 256;
+            int targetHeight = 256;
+            Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
+                    targetHeight,Bitmap.Config.ARGB_8888);
+
+            Canvas canvas = new Canvas(targetBitmap);
+            Path path = new Path();
+            path.addCircle(((float) targetWidth - 1) / 2,
+                    ((float) targetHeight - 1) / 2,
+                    (Math.min(((float) targetWidth),
+                            ((float) targetHeight)) / 2),
+                    Path.Direction.CCW);
+
+            canvas.clipPath(path);
+            Bitmap sourceBitmap = scaleBitmapImage;
+            canvas.drawBitmap(sourceBitmap,
+                    new Rect(0, 0, sourceBitmap.getWidth(),
+                            sourceBitmap.getHeight()),
+                    new Rect(0, 0, targetWidth,
+                            targetHeight), null);
+            return targetBitmap;
+    }
+
+    public byte[] bitmapToByte(Bitmap bmp){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
+    public Bitmap byteToBitmap(byte[] bytes){
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);;
+        return bitmap;
+    }
+
+}
---
+++ app/src/main/java/com/example/newpc/qrcode/Retrofit/APIInterface.java
@@ -0,0 +1,36 @@
+package com.example.newpc.qrcode.Retrofit;
+
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.HTTP;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface APIInterface {
+    /*
+    @HTTP(method = "GET", path = "/account/{id}")
+    Call<ResponseTemplate> doGetUser(@Path("id") String id);
+
+    @HTTP(method = "GET", path = "/parking_lot/list_parking_slot")
+    Call<ResponseTemplate> doGetCarparkSlots(@Query("parkingLotId") int parkingLotId);
+
+    @HTTP(method = "GET", path = "/driver/all_parking_lot")
+    Call<ResponseTemplate> doGetAllParkingLot();
+
+    @HTTP(method = "POST", path = "/driver/create_driver", hasBody = true)
+    Call<ResponseTemplate> doSubmitUser(@Body Account account);
+
+    @HTTP(method = "POST", path = "/account/login", hasBody = true)
+    Call<ResponseTemplate> doCheckLogin(@Body UserLogin userLogin);
+
+    @HTTP(method = "POST", path = "/driver/forget_password", hasBody = true)
+    Call<ResponseTemplate> doForgetPassword(@Query("email") String email);
+
+    @HTTP(method = "PUT", path = "/account/update/{id}", hasBody = true)
+    Call<ResponseTemplate> doUpdateUser(@Path("id") Integer id, @Body InformationAccount account);
+
+    @HTTP(method = "PUT", path = "/account/change_password", hasBody = true)
+    Call<ResponseTemplate> doChangePassword(@Body PasswordChanger changePassword);
+    */
+}
